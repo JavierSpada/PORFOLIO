438e2f9a4ab2ddb21646502ee17fabcd9d970d1d
Javier <spadajavier@gmail.com>
2022-04-19 00:23:06 -0300

    creacion componentes, hasta aca anda

--- frontendAngular/src/app/components/item/item.component.ts
+++ frontendAngular/src/app/components/item/item.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, Input } from '@angular/core';
+import { ExperienciaLaboralService } from 'src/app/service/experiencia-laboral.service';
+import { ItemElement } from '../ItemElement';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
+  @Input() titulo: string = "";
+  @Input() descripcion: string = "";
+  @Input() fecha: string = "";
+  @Input() urlImagen: string = "";
 
-  constructor() { }
+
+  constructor(
+    private experienciaLaboralService: ExperienciaLaboralService
+  ) { }
 
   ngOnInit(): void {
+  
+  }
+
+  editar() {
+    console.log(" dfasdfasdfasdf");
+  }
+
+  eliminar(item : ItemElement) {
+
   }
 
 }
